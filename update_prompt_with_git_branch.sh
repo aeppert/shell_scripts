af5700e99fd2462daa55be643c31a7532e755372
Aaron Eppert <aeppert@gmail.com>
2016-09-06 16:42:01 -0400

    Create update_prompt_with_git_branch.sh

---
+++ update_prompt_with_git_branch.sh
@@ -0,0 +1,9 @@
+function git-branch-name {
+  git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3-
+}
+
+function git-branch-prompt {
+  local branch=`git-branch-name`
+  if [ $branch ]; then printf " [%s]" $branch; fi
+}
+PS1="\u@\h \[\033[0;36m\]\W\[\033[0m\]\[\033[0;32m\]\$(git-branch-prompt)\[\033[0m\] \$ "
